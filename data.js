54499f50bec6728041ba2a768db6951cd49c645f
chowden-a <103084809+chowden-a@users.noreply.github.com>
2022-04-06 23:53:31 +0800

    First commit

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.566912011892443,
+        "pitch": 0.12459963247167849,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5316136298156078,
+          "pitch": 0.08172270640926804,
+          "rotation": 5.497787143782138,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.097776622735161,
+          "pitch": -0.014291485231352041,
+          "title": "Starbucks entrance",
+          "text": "Starbucks is open 24/7 here! Inside you can find cold drinks, hot drinks and stressed students."
+        },
+        {
+          "yaw": -1.7961324782694383,
+          "pitch": -0.12605716943581413,
+          "title": "Vending Machines",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.60925746715278,
+          "pitch": 0.014792409645945526,
+          "title": "View of utown green",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.1166695778334805,
+        "pitch": 0.0583411397820246,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3983388668045453,
+          "pitch": 0.07638731689285194,
+          "rotation": 0,
+          "target": "2-erc_level-1_tembusu-tree"
+        },
+        {
+          "yaw": -1.2089516294791913,
+          "pitch": 0.07251883674481796,
+          "rotation": 0,
+          "target": "0-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3782408971450835,
+          "pitch": -0.010400357712683217,
+          "title": "Escalator to ERC rooftop",
+          "text": "The rooftop is a nice venue to hang out when the weather is good&nbsp; :)"
+        },
+        {
+          "yaw": 1.526552518521843,
+          "pitch": 0.016921729148712927,
+          "title": "Walk way to the utown gym",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3670810455474687,
+          "pitch": -0.001080736129301485,
+          "title": "Walkway to New College",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.8529330598116545,
+        "pitch": 0.15260458204140726,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.642118759180919,
+          "pitch": 0.10222538697649952,
+          "rotation": 0,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0448045313949628,
+          "pitch": 0.24689741549255828,
+          "title": "The Tembusu Tree",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7574402742546624,
+          "pitch": 0.035802191943203,
+          "title": "A toilet",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Charlton's Utown Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
